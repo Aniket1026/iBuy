61558f6077166a4a906576e4aab7a395c8b4901c
Aniket <annudubey1026@gmail.com>
2023-05-15 19:01:24 +0530

    product model created

---
+++ server/model/productModel.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter product name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter product description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter product price"],
+    maxLength: [8, "price cannot exceed 8 characters"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please enter product category"],
+  },
+  Stock: {
+    type: String,
+    required: [true, "Please enter product stock"],
+    maxLength: [4, "Product stock cannot exceed limit of 1000"],
+    default: 1,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
