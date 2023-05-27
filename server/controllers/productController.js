110fc07d9cf25d21d0717516ad8994bdf644f6f4
Aniket <annudubey1026@gmail.com>
2023-05-28 01:30:04 +0530

    try catch blocks for error handling

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -3,45 +3,56 @@ import CustomError from "../utils/CustomError.js";
 
 // -- Admin
 export const createProducts = async (req, res) => {
-  const product = await Product.create(req.body);
-  res.status(201).json({ success: true, product });
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({ success: true, product });
+  } catch (error) {
+    res.status(500).json({ msg: "error in product creating" + error });
+  }
 };
 
 export const getAllProducts = async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({ success: true, products });
+  try {
+    const products = await Product.find();
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    res.status(500).json({ msg: "Cannot fetch all products " + error });
+  }
 };
 
 // -- get unique product details
 
-export const productDetails = async(req,res,next) => {
+export const productDetails = async (req, res, next) => {
   try {
-    const product = await Product.findById(req.params.id)
+    const product = await Product.findById(req.params.id);
     if (!product) {
-      return next(new CustomError("product not found",404))
+      return next(new CustomError("product not found", 404));
     }
-   return res.status(200).json({ success: true, product })
-    
+    return res.status(200).json({ success: true, product });
   } catch (error) {
-    return res.status(500).json(error)
+    return res.status(500).json(error);
   }
-}
+};
 
 // -- update product
 export const productUpdate = async (req, res) => {
-  let product = await Product.findById(req.params.id);
+  try {
+    let product = await Product.findById(req.params.id);
 
-  if (!product) {
-    res.status(500).json({ success: false, message: "Product not found" });
+    if (!product) {
+      res.status(500).json({ success: false, message: "Product not found" });
+    }
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    });
+
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    res.status(500).json({ msg: "Cannot update product details " + error });
   }
-
-  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
-
-  res.status(200).json({ success: true, product });
 };
 
 // Delete product
