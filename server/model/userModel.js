c5e9cf1c3080707a7c64e8abac01c1850debe519
Aniket <annudubey1026@gmail.com>
2023-05-28 16:30:24 +0530

    user model created

---
+++ server/model/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userScehma = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [2, "Name should have more than 2 characters"],
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter username"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please enter email"],
+    validate: [validator.isEmail, "please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: [6, "password should be more than 6 characters"],
+    select: false,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  role: {
+    type: String,
+    default: "user",
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
+
+const User = mongoose.model("User", userScehma);
+export default User;
