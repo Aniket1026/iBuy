ad11b6a500874fbd7eacc49fe8e60326a9eefa97
Aniket <annudubey1026@gmail.com>
2023-09-01 22:13:37 +0530

    admin route and Implement error handling with try-catch blocks

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,23 +1,37 @@
-import jwt from "jsonwebtoken"
-import User from "../model/userModel.js"
-import CustomError from "../utils/CustomError.js"
+import jwt from "jsonwebtoken";
+import User from "../model/userModel.js";
+import CustomError from "../utils/CustomError.js";
 
-export const isUserAuthenticated = async(req,res,next) => {
-    const { token } = req.cookies
+export const isUserAuthenticated = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
     if (!token) {
-        return next(new Error("Please login to access this resource "))
+      return next(new Error("Please login to access this resource "));
     }
     const decodedData = jwt.verify(token, process.env.SECRET_KEY);
-    req.user = await User.findById(decodedData.id)
-    next()
-}
+    req.user = await User.findById(decodedData.id);
+    next();
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, msg: "error in isUserAuthenticated" });
+  }
+};
 
 export const authorizeRoles = (...roles) => {
+  try {
     return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            res.status(403)
-            return next( new CustomError(`Role ${req.user.role} is not allowed to access this resource`))
-        }
-        next()
-    }
-}
+      if (!roles.includes(req.user.role)) {
+        res.status(403);
+        return next(
+          new CustomError(
+            `Role ${req.user.role} is not allowed to access this resource`
+          )
+        );
+      }
+      next();
+    };
+  } catch (error) {
+    res.status(500).json({ success: false, msg: "error in authorizeRoles" });
+  }
+};
