6ee2785c46bcf370cabe8cb2d9b434a00bb10595
Aniket <annudubey1026@gmail.com>
2023-09-22 15:36:28 +0530

    Configured material-tailwind for Carousel feature

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,12 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import "./index.css";
 import store from "./app/store.jsx";
+import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </Provider>
 );
