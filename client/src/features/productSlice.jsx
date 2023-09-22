499f046062cdb3ee29371aab566333909a39c7f4
Aniket <annudubey1026@gmail.com>
2023-09-22 15:31:11 +0530

    Implemented reducer for product details

--- client/src/features/productSlice.jsx
+++ client/src/features/productSlice.jsx
@@ -1,10 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+const URL = "http://localhost:5000/api/v1";
+
 export const productsSlice = createSlice({
   name: "products",
   initialState: {
     products: [],
+    productDetails: null,
     status: "loading",
     error: null,
   },
@@ -22,13 +25,36 @@ export const productsSlice = createSlice({
       .addCase(fetchProducts.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+      })
+      .addCase(fetchProductDetails.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductDetails.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.productDetails = action.payload;
+      })
+      .addCase(fetchProductDetails.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
       });
   },
 });
 
 export const fetchProducts = createAsyncThunk("products/fetchAll", async () => {
-  const response = await axios.get("http://localhost:5000/api/v1/products");
+  const response = await axios.get(`${URL}/products`);
   return await response.data.products;
 });
+
+export const fetchProductDetails = createAsyncThunk(
+  "products/fetchProductDetails",
+  async (productId) => {
+    try {
+      const response = await axios.get(`${URL}/product/${productId}/`);
+      return await response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+);
 
 export default productsSlice.reducer;
