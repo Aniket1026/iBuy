385380bf51beada9dd0684382f573e32472f29af
Aniket <annudubey1026@gmail.com>
2023-11-01 19:00:33 +0530

    Feature: Add Cart Reducer to Store

--- client/src/app/store.jsx
+++ client/src/app/store.jsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from '../features/productSlice.jsx'
 import authReducer from '../features/authSlice.jsx'
 import userReducer from '../features/userSlice.jsx'
+import cartReducer from '../features/cartSlice.jsx'
 
 export default configureStore({
   reducer: {
     products: productsReducer,
     auth: authReducer,
     user: userReducer,
+    cart: cartReducer,
   },
 });
 
