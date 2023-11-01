b6481cbb18dbd1d08df6199488b5744eae0f7c24
Aniket <annudubey1026@gmail.com>
2023-11-02 00:35:39 +0530

    Refactor: Adjust cartItemCard Component for Updated Redux States

--- client/src/components/Cart/cartItemCard.jsx
+++ client/src/components/Cart/cartItemCard.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./cartItemCard.css";
 
-const CartItemCard = ({ item, deleteCartItems }) => {
+const CartItemCard = ({ item }) => {
   return (
     <div className="CartItemCard">
-      <img src={item.image} alt="ssa" />
+      <img src={item.response.product.images[0].url} alt="ssa" />
       <div>
         <Link to={`/product/${item.product}`}>{item.name}</Link>
-        <span>{`Price: ₹${item.price}`}</span>
-        <p onClick={() => deleteCartItems(item.product)}>Remove</p>
+        <span>{`Price: ₹${item.response.product.price}`}</span>
+        <p>Remove</p>
       </div>
     </div>
   );
